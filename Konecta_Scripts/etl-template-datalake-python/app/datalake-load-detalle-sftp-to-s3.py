48c452486b78bbd7b0866e1304e0b88bcc501e03
Robert Vivar <dlanegraor@intercorp.com.pe>
2024-09-14 17:45:52 -0500

    Mejoras en los files de python

--- Konecta_Scripts/etl-template-datalake-python/app/datalake-load-detalle-sftp-to-s3.py
+++
@@ -1,8 +0,0 @@
-from package.etl_upload import Put
-
-# for sftp
-file = Put(id_table=None, id_query=None, id_date=0, id_file=None, id_column=None, id_secret=0,
-           id_service=5, id_mail=0, id_sftp=0)
-file.upload_file_sftp_tos3()
-
-print("successful process")
--- Konecta_Scripts/etl-template-datalake-python/app/package/function/get_column.py
+++
@@ -1,7 +0,0 @@
-from .get_config import ConfigJson
-
-def get_column_map(id_columns) -> dict:
-    columns = ConfigJson().get_content_json(file_json="column")["columns"][id_columns]
-    param_column_map = columns["column"]
-
-    return param_column_map
